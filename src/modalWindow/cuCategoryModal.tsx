6849cfe7dfaa4f630e63c6a0b99517b385aed88b
dicollarsfamily <dicollarsfamily@gmail.com>
2021-03-01 10:08:39 +0300

    Added form validation

--- src/modalWindow/cuCategoryModal.tsx
+++ src/modalWindow/cuCategoryModal.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import '../modalWindow/modal.css';
 import { Formik, Field, Form } from "formik";
 import { CUCategoryModalProps } from '../interfaces';
 import { ImCross } from 'react-icons/im';
+import * as yup from 'yup';
 
 function CUCategoryModal(props: CUCategoryModalProps) {
+    const validationsSchema = yup.object().shape({
+        name: yup.string().required('Поле обязательно.')
+    });
 
     return (
         <div className='modal-overlay'>
@@ -16,23 +19,27 @@ function CUCategoryModal(props: CUCategoryModalProps) {
                     onSubmit={(values: any) => {
                         props.handleSubmit(values);
                     }}
+                    validationSchema={validationsSchema}
                 >
-                    <Form>
-                        <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginLeft: '24px', marginTop: '42px', marginBottom: '34px' }}>
-                            <label>Имя<span style={{ color: 'red', marginLeft: '+2px' }}>*</span></label>
-                            <Field className='field-name-category' name='name' type='text' maxLength='255' placeholder='Введите имя категории' />
-                        </div>
+                    {({ errors, touched }) => (
+                        <Form>
+                            <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginLeft: '24px', marginTop: '42px', marginBottom: '34px' }}>
+                                <label>Имя
+                                {errors.name && touched.name ? (<span style={{ color: 'red', marginLeft: '+2px' }}>*</span>) : null}
+                                </label>
+                                <Field className='field-name-category' name='name' type='text' maxLength='255' placeholder='Введите имя категории' />
+                            </div>
 
-                        <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginLeft: '24px'}}>
-                            <label>Описание</label>
-                            <Field className='field-desc-category' name='description' type='text' maxLength='512' placeholder='Введите описание категории' />
-                        </div>
+                            <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginLeft: '24px' }}>
+                                <label>Описание</label>
+                                <Field className='field-desc-category' name='description' type='text' maxLength='512' placeholder='Введите описание категории' />
+                            </div>
 
-                        <div>
-                            <button className='create-category-button' type='submit'>{props.submitButtonText}</button>
-                            <button className='close-category-button' onClick={props.onCloseButtonClick}>Закрыть</button>
-                        </div>
-                    </Form>
+                            <div>
+                                <button className='create-category-button' type='submit'>{props.submitButtonText}</button>
+                                <button className='close-category-button' onClick={props.onCloseButtonClick}>Закрыть</button>
+                            </div>
+                        </Form>)}
                 </Formik>
             </div>
         </div >);
--- src/modalWindow/cuTaskModal.tsx
+++ src/modalWindow/cuTaskModal.tsx
@@ -4,9 +4,13 @@ import '../modalWindow/modal.css';
 import { Formik, Field, Form } from "formik";
 import { CUTaskModalProps, Category, State } from '../interfaces';
 import { ImCross } from 'react-icons/im';
+import * as yup from 'yup';
 
 function CUTaskModal(props: CUTaskModalProps) {
     const list: Array<Category> = useSelector((state: State) => state.Categories);
+    const validationsSchema = yup.object().shape({
+        name: yup.string().required('Поле обязательно.')
+    });
 
     return (
         <div className='modal-overlay'>
@@ -18,33 +22,37 @@ function CUTaskModal(props: CUTaskModalProps) {
                     onSubmit={(values) => {
                         props.handleSubmit(values);
                     }}
+                    validationSchema={validationsSchema}
                 >
-                    <Form className='form-padding'>
-                        <div >
-                            <div className='form-group' style={{ display: 'inline-block', position: 'relative' }}>
-                                <label>Имя<span style={{ color: 'red', marginLeft: '+2px' }}>*</span></label>
-                                <Field name='name' type='text' maxLength='255' placeholder='Введите имя задания' className='field-name' />
+                    {({ errors, touched }) => (
+                        <Form className='form-padding'>
+                            <div >
+                                <div className='form-group' style={{ display: 'inline-block', position: 'relative' }}>
+                                    <label>Имя
+                                {errors.name && touched.name ? (<span style={{ color: 'red', marginLeft: '+2px' }}>*</span>) : null}
+                                    </label>
+                                    <Field name='name' type='text' maxLength='255' placeholder='Введите имя задания' className='field-name' />
+                                </div>
+                                <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginLeft: '+16px', marginBottom: '-18px' }}>
+                                    <label>Категория</label>
+                                    <Field name="categoryId" as="select" className='field-category-task'>
+                                        <option value='0' className='font-color-gray' >Выберите категорию</option>
+                                        {list?.map((category: Category) =>
+                                            <option key={category.id} value={category.id} >{category.name}</option>
+                                        )}
+                                    </Field>
+                                </div>
                             </div>
-                            <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginLeft: '+16px', marginBottom: '-18px' }}>
-                                <label>Категория</label>
-                                <Field name="categoryId" as="select" className='field-category-task'>
-                                    <option value='0' className='font-color-gray' >Выберите категорию</option>
-                                    {list?.map((category: Category) =>
-                                        <option key={category.id} value={category.id} >{category.name}</option>
-                                    )}
-                                </Field>
+                            <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginTop: '+24px' }}>
+                                <label>Описание</label>
+                                <Field className='field-desc-task' name='description' type='text' maxLength='1536' placeholder='Введите описание задания' />
                             </div>
-                        </div>
-                        <div className='form-group' style={{ display: 'inline-block', position: 'relative', marginTop: '+24px' }}>
-                            <label>Описание</label>
-                            <Field className='field-desc-task' name='description' type='text' maxLength='1536' placeholder='Введите описание задания' />
-                        </div>
 
-                        <div>
-                            <button className='create-task-button' type='submit'>{props.submitButtonText}</button>
-                            <button className='close-task-button' onClick={props.onCloseButtonClick}>Закрыть</button>
-                        </div>
-                    </Form>
+                            <div>
+                                <button className='create-task-button' type='submit'>{props.submitButtonText}</button>
+                                <button className='close-task-button' onClick={props.onCloseButtonClick}>Закрыть</button>
+                            </div>
+                        </Form>)}
                 </Formik>
             </div>
         </div >);
